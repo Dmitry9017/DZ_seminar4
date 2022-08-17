edd6a325e95b30abf2591ac2a99e1c0d8a28281c
Dmitry <dima_gazavto@mail.ru>
2022-08-17 19:22:40 +0300

    All task completed

---
+++ Task25/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.Write("Введите число A: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int result = num1;
+for(int i = 1; i < num2; i++){
+    result = result * num1;
+}
+Console.WriteLine($"Число {num1} возведенное в степень {num2} равно {result}");
+
+/*Но для оптимизации лучше написать такой код
+int result = Convert.ToInt32(Math.Pow(num1, num2));
+Console.WriteLine($"Число {num1} возведенное в степень {num2} равно {result}");*/
---
+++ Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Write("Введите любое число: ");
+int number = int.Parse(Console.ReadLine());
+string number1 = Convert.ToString(number);
+
+int sum = 0;
+int number2 = number;
+for(int i = 0; i < number1.Length; i++){
+    sum += number2%10;
+    number2 = number2 / 10;
+}
+Console.WriteLine($"Сумма цифр числа {number} равна {sum}");
