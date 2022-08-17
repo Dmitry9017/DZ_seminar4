bba8dd53f79a0cacc44ea071fc77c8ccb6140c5b
Dmitry <dima_gazavto@mail.ru>
2022-08-17 19:28:43 +0300

    Новый коммит

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -8,9 +8,20 @@ int number = int.Parse(Console.ReadLine());
 string number1 = Convert.ToString(number);
 
 int sum = 0;
-int number2 = number;
+int number2 = number; //Переменная number2 нужна для корректного вывода числа пользователя number в консоли
 for(int i = 0; i < number1.Length; i++){
     sum += number2%10;
     number2 = number2 / 10;
 }
 Console.WriteLine($"Сумма цифр числа {number} равна {sum}");
+
+/*Просьба пояснить почему не код не отрабатывает, если действовать через массив
+Console.Write("Введите любое число: ");
+int number = int.Parse(Console.ReadLine());
+string number1 = Convert.ToString(number);
+
+int sum = 0;
+for(int i = 0; i < number1.Length; i++){
+    sum += number[i];
+}
+Console.WriteLine($"Сумма цифр числа {number} равна {sum}");*/
